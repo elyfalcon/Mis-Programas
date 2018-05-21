514eec82c68f2f988135cbd6595b6a8f73b9aa2b
elyfalcon <elyfalcon@yahoo.com>
2018-05-21 03:09:15 -0300

    listoo

--- PrimerParcial/funciones.h.save
+++ PrimerParcial/funciones.h.save
@@ -7,7 +7,8 @@
 
 
 
-void AltaUnaxxxx(ePropietario lista_propietario[]);
+void AltaUnaxxxx(ePropietario propietario[],int);
+void AltaPropietarios(ePropietario lista[],int limite);
 int obtenerEspacioLibre(ePropietario lista_propietario[],int cantidad);
 int obtenerLibreIngreso(eIngresos lista_ingresos[],int );
 void IngresoAuto(eIngresos lista_ingresos[],int,ePropietario lista_propietario[],int);
@@ -16,12 +17,10 @@ int Inicializa_Propietario(ePropietario lista_propietario[],int limite);
 void MostrarUnaPersona(ePropietario propietario);
 void MostrarPropietarioConSusAutos(ePropietario lista_prop[],int cant_prop,eIngresos lista_ingre[],int cant_ingr);
 void MostrarPropietariosAutosAudi(ePropietario lista_prop[],int cant_prop,eIngresos lista_ingre[],int cant_ingr);
-int ListarPropietarios(ePropietario lista_propietario[],int cantidad);
+int ListarPropietarios(ePropietario lista_propietario[],int );
+void ListarPropietarioYAuto(ePropietario lista[],eIngresos ingreso[],int,int);
 void BajaPropietario(ePropietario lista_prop[],int indice,int cant_prop,eIngresos list_ingresos[],int cant_ingreso,eEgreso lista_egreso[], int cant_egreso);
-//int buscarPorId(ePropietario lista_propietario[], int id,int cantidad);
 int buscarPorId(ePropietario lista[], int ,int );
-//int buscarPorId(eIngresos lista[], int ,int );
-//int buscarIngreso(eIngresos lista_ingresos[],int id,int cantidad);
 void ModificarUnaPersona(ePropietario lista_propietario[],int cantidad);
 void MostrarIngresoAuto(eIngresos autos);
 void ListarIngresoAuto(eIngresos lista_ingresos[],int);
@@ -36,5 +35,10 @@ void AltaEgreso(eEgreso vec[],int,int,int);
 void ListarUnEgreso(eEgreso autos);
 void ListarEgresos(eEgreso vec[],int);
 void RecaudacionTotal(eEgreso lista[],int limite);
-//void IngresoAuto(eIngresos autos);
+void RecaudacionPorMarca(eEgreso lista[],int limite);
+void ListarAutosYPropietarios(eIngresos lista[],int,ePropietario vec[],int);
+void ListarPropActivos(ePropietario lista[],int);
+void ListarPropBajas(ePropietario lista[],int);
+void MostrarIngresoPorid(eIngresos autos[],int);
+float Liquidacion(eIngresos vec[],eEgreso egreso[],int,int);
 #endif // FUNCIONES_H_INCLUDED
--- PrimerParcial/main.c
+++ PrimerParcial/main.c
@@ -6,6 +6,7 @@
 #include "ingresos.h"
 #include "egresos.h"
 #define tam_ingresos 20
+#define cant_propietarios 20
 #define ALPHA_ROMEO 1
 #define FERRARI 2
 #define AUDI 3
@@ -16,15 +17,15 @@ int main()
     char seguir='s';
     int opcion;
     int i;
-    ePropietario lista_propietario[30];
+    ePropietario lista_propietario[cant_propietarios];
     ePropietario propietario;
     int idPropietario;
     eIngresos lista_ingresos[tam_ingresos];
     eIngresos autos;
     eEgreso lista_egreso[30];
-    Inicializa_Propietario(lista_propietario,30);
-    Inicializa_Autos(lista_ingresos,20);
-    Inicializa_Egreso(lista_egreso,20);
+    Inicializa_Propietario(lista_propietario,cant_propietarios);
+    Inicializa_Autos(lista_ingresos,tam_ingresos);
+    Inicializa_Egreso(lista_egreso,tam_ingresos);
     HardcodePersona(lista_propietario);
     HardcodeIngresos(lista_ingresos);
     HardcodeEgreso(lista_egreso);
@@ -50,36 +51,34 @@ int main()
         switch(opcion)
         {
             case 1:
-                 AltaUnaxxxx(lista_propietario);
+                 AltaUnaxxxx(lista_propietario,cant_propietarios);
                 break;
             case 2:
-                ModificarUnaPersona(lista_propietario,30);
+                ModificarUnaPersona(lista_propietario,cant_propietarios);
                 break;
             case 3:
-                MostrarPropietarios(lista_propietario,tam_ingresos);
-                idPropietario=PedirEntero("\nIngrese el id del Propietario a borrar: ");
-                i=buscarPorId(lista_propietario,idPropietario,tam_ingresos);
-                BajaPropietario(lista_propietario,i,10,lista_ingresos,tam_ingresos,lista_egreso,20);
+                BajaPropietario(lista_propietario,i,10,lista_ingresos,tam_ingresos,lista_egreso,tam_ingresos);
                 break;
             case 4:
-                IngresoAuto(lista_ingresos,tam_ingresos,lista_propietario,20);
+                IngresoAuto(lista_ingresos,tam_ingresos,lista_propietario,tam_ingresos);
                 break;
             case 5:
-                EgresoAutos(lista_ingresos,tam_ingresos,lista_egreso,20,lista_propietario,10);
+                EgresoAutos(lista_ingresos,tam_ingresos,lista_egreso,20,lista_propietario,tam_ingresos);
                 break;
             case 6:
-                RecaudacionTotal(lista_egreso,20);
+                RecaudacionTotal(lista_egreso,tam_ingresos);
                 break;
             case 7:
+                RecaudacionPorMarca(lista_egreso,tam_ingresos);
                 break;
             case 8:
-
-                MostrarPropietarioConSusAutos(lista_propietario,20,lista_ingresos,10);
+                ListarPropietarioYAuto(lista_propietario,lista_ingresos,20,tam_ingresos);
                 break;
             case 9:
                 MostrarPropietariosAutosAudi(lista_propietario,tam_ingresos,lista_ingresos,tam_ingresos);
                 break;
             case 10:
+                ListarAutosYPropietarios(lista_ingresos,tam_ingresos,lista_propietario,20);
                 break;
             case 11:
                 seguir = 'n';
